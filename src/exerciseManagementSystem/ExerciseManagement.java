ae14e6eaad8a728ea1d2fc8c272383c914db26ee
choi-su-min <jcr60638@gmail.com>
2021-03-14 15:00:36 +0900

    added exercisemanager.java

---
+++ src/exerciseManagementSystem/ExerciseManagement.java
@@ -0,0 +1,13 @@
+package exerciseManagementSystem;
+
+public class ExerciseManagement {
+	
+	public static void main(String[] args) {
+		System.out.println("1. division way");
+		System.out.println("2. meal way");
+		System.out.println("3. physically fit");
+		System.out.println("4. position way");
+		System.out.println("5. exit");
+	}
+
+}
