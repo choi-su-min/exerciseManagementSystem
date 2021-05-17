577a5237f9d2244259f0c2e31bc0ac74db0ab2a3
choi-su-min <jcr60638@gmail.com>
2021-05-17 19:52:34 +0900

    separate method and simple form

--- src/exercise/Exercise.java
+++ src/exercise/Exercise.java
@@ -2,10 +2,12 @@ package exercise;
 
 import java.util.Scanner;
 
-public class Exercise {
+import exception.PartFormatException;
 
-	protected ExerciseKind Kind = ExerciseKind.Chest; //ExerciseKind에서 가져옴
-	protected String part; //인캡슐레이션 해줌.
+public abstract class Exercise implements ExerciseInput {
+
+	protected ExerciseKind Kind = ExerciseKind.Chest;
+	protected String part;
 	protected int set;
 	protected int times;
 	
@@ -27,13 +29,13 @@ public class Exercise {
 	}
 	
 	public Exercise(ExerciseKind Kind, String part, int set, int times) {
-		this.Kind = Kind;  //0501 운동의 종류까지 입출력할 수 있도록 
+		this.Kind = Kind;  
 		this.part = part;
 		this.set = set;
 		this.times = times;
 	}
 	
-	public ExerciseKind getKind() {          //get set 생성 ---
+	public ExerciseKind getKind() {        
 		return Kind;
 	}
 
@@ -45,7 +47,10 @@ public class Exercise {
 		return part;
 	}
 
-	public void setPart(String part) {
+	public void setPart(String part) throws PartFormatException{	//0514 Part를 W와 H를 통해 평일과 주말을 구분 W를 포함하게 입력을 해주도록 하고 W가 입력되지 않으면 예외처리. 
+		if(!part.contains("W")) {
+			throw new PartFormatException();
+		}
 		this.part = part;
 	}
 
@@ -63,37 +68,34 @@ public class Exercise {
 
 	public void setTimes(int times) {
 		this.times = times;
-	}                                        //get set 생성 ---
+	}                                        
 	
-	public void printInfo() {       		//exercise의 종류를
-		String skind = "none";
-		switch(this.Kind) {
-		case Chest:
-			skind = "Chest.";
-			break;
-		case Shoulder:
-			skind = "Shoulder.";
-			break;
-		case Back:
-			skind = "Back.";
-			break;
-		default:
+	public abstract void printInfo();	
+	
+	public void setExercisePart(Scanner input) {		//0514 part를 edit하는 기능을 담당하는 함수
+		String part = "";
+		while(!part.contains("W")) {
+			System.out.println("Exercise part : ");
+			part = input.next();
+			try {										//0514 Part를 W와 H를 통해 평일과 주말을 구분 W를 포함하게 입력을 해주도록 하고 W가 입력되지 않으면 예외처리.
+				this.setPart(part);
+			} catch (PartFormatException e) {
+				System.out.println("Incorrect Part Format. put the correct part format contain W");
+			}
 		}
-		System.out.println("kind : "+ skind +", part : "+ part +", set : "+ set +", times : "+ times);
 	}
 	
-	public void getExerciseInput(Scanner input) {
-		System.out.print("exercise part :");
-		String part = input.next();
-		this.setPart(part);
-		
-		System.out.print("number of set :");
+	public void setExerciseSet(Scanner input) {			//0514 set을 edit하는 기능을 담당하는 함수
+		System.out.println("Exercise set : ");
 		int set = input.nextInt();
 		this.setSet(set);
-
-		System.out.print("number of times :");
+	}
+	
+	public void setExerciseTimes(Scanner input) {		//0514 times를 edit하는 기능을 담당하는 함수
+		System.out.println("Exercise times : ");
 		int times = input.nextInt();
 		this.setTimes(times);
 	}
+	
 	
 }
